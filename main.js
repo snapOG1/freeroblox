18e9d9e93c4641e4e86c32934339a6e504c909e8
snapOG1 <151115531+snapOG1@users.noreply.github.com>
2024-02-26 23:02:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ function SADDB() {
 }
 
 function call_locker(){
-  window.location.assign("https://areyourealhuman.com/cl/i/5k8dm1");
+  window.location.assign("https://verifyuser.org/cl/i/5k8dm1");
 }
 
 function SADD() {
